643fd62b9b2d005eb559e401e1975756d00c4bf6
Bárbara Hellen <barbarahellen993@gmail.com>
2024-01-24 14:27:32 -0300

    exercícios

---
+++ javascript/exercícios/relógio/script.js
@@ -0,0 +1,28 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+function rotacionarPonteiro(ponteiro, angulo){
+    ponteiro.style.transform = `rotate(${angulo}deg)`
+}
+
+function relogio(){
+    date = new Date();
+
+    let s = date.getSeconds() * 6;
+    let m = date.getMinutes() * 6 + (s / 60);
+    let h = date.getHours() * 30 + (m / 12);
+
+    rotacionarPonteiro(segundos, s);
+    rotacionarPonteiro(minutos, m);
+    rotacionarPonteiro(horas, h);
+
+}
+
+setInterval(relogio, 1000);
+
+let date = new Date();
+
+// console.log(date.getSeconds());//segundos
+// console.log(date.getMinutes());//minutos
+// console.log(date.getHours());//horas
